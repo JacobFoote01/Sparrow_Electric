49039928c69aed2fc55b3592071a3278888c7d17
Jacob Foote <jacobjfoote01@gmail.com>
2024-04-30 12:46:01 -0500

    made some styling changes

---
+++ src/TopFooter.jsx
@@ -0,0 +1,18 @@
+
+const TopFooter = () => {
+    return(
+        <>
+        <h2>
+            Go Solar Today
+        </h2>
+        <p>
+            Pay less for your solar panels than the monthly cost of your power bill and reduce your dependence on traditional sources of electricity with Sparrow Electric.
+        </p>
+        <button>
+            Let's Work Together
+        </button>
+        </>
+    )
+}
+
+export default TopFooter
