50a05b921c6ffa8768f7c00dd8a47ea3c2a614fe
Jacob Foote <jacobjfoote01@gmail.com>
2024-04-29 23:20:44 -0500

    added more content to the home page

--- src/LowerHeader.jsx
+++ src/LowerHeader.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 const LowerHeader = () => {
     return(
         <header>
-            <img className='neighborhood'></img>
+            <a>SPARROW ELECTRIC</a>
             <a href=''> How We Help </a>
             <a href=''> What We Offer </a>
             <a href=''> Financial Help Center </a>
             <a href=''> Why Sparrow Solar </a>
             <button>Request a Quote</button>
+            <br/>
+            <>Give us a call today and we will waive all financing fees.</>
+            <a href=''> See Details </a>
         </header>
     )
 }
